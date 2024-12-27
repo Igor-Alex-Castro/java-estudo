e09083687dd28016bbdd4260491958d1353380ef
Igor Alex de Castro <igoralex8701@gmail.com>
2024-12-27 12:39:27 -0300

    Expressões artiméticas

---
+++ src/ExpressoesAritmeticas.java
@@ -0,0 +1,15 @@
+
+public class ExpressoesAritmeticas {
+	public static void main(String[] args) {
+		//Expressões aritméticas
+		//expressão -> resultado -  valor numerico
+		//exemplo: 4 + 5 = 9
+		//operadores: +, -, *, /, %(mod)
+		//precedencia:
+			//1° -> ()
+			//2° -> *, /, %
+		    //3° ->  +, -
+		
+		
+	}
+}
