1a29c179f1a7453de9dadbd32b774a293a7d7720
Igor Alex de Castro <igoralex8701@gmail.com>
2024-12-27 12:55:58 -0300

    Variáveis

--- src/ExpressoesAritmeticas.java
+++ src/ExpressoesAritmeticas.java
@@ -1,15 +1,47 @@
 
 public class ExpressoesAritmeticas {
 	public static void main(String[] args) {
-		//Expressões aritméticas
-		//expressão -> resultado -  valor numerico
-		//exemplo: 4 + 5 = 9
-		//operadores: +, -, *, /, %(mod)
-		//precedencia:
-			//1° -> ()
-			//2° -> *, /, %
-		    //3° ->  +, -
+		/* VISÃO GERAL
+		 * - Um programa de computador em execução lida com dados.
+		 * - Como esses dados são armazenados
+		 * - Em VARIÁVEIS!
+		 * 
+		 * Denifisão informal:
+		 * 	Em programação, uma variável é uma porção de memória (RAM) utilizada para armazenar
+		 *  dados durante a execução dos programas.
+		 *  
+		 * SINTAXE
+		 *  Essas variáveis precisam ser declarada
+		 *  <tipo><nome> = <valor inicial>
+		 *  				opcional
+		 * */
+		//Exemplo
+		int idade = 25;
+		double altura = 1.68;
+		char sexo = 'F';
 		
+		/* Uma variável possui:
+		 * - nome
+		 * - tipo
+		 * - valor
+		 * - endereco
+		 * */
 		
+		/* TIPOS PRIMITIVOS
+		 * -----------------------------------------------------
+		 * Um bit pode armazenar 2 valores possíveis (0 ou 1)
+		 * Cada bit = 2 possibilidades
+		 * 8 bits: 2^8 = 256 possibilidades
+		 * ----------------------------------------------------
+		 * byte - 8 bits
+		 * short - 16 bits
+		 * int - 32 bits
+		 * long - 64 bits
+		 * float - 32 bits
+		 * double - 64 bits
+		 * char - 16 bits
+		 * boolean - 1 bit
+		 * ----String -> cadeia de caracteres(palavras ou textos)
+		 * */
 	}
 }
