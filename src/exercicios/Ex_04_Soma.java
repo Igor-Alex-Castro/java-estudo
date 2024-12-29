6ac6186e744bb888d6f1ef765f904ff61dc90c94
Igor Alex de Castro <igoralex8701@gmail.com>
2024-12-29 17:46:34 -0300

    soma

---
+++ src/exercicios/Ex_04_Soma.java
@@ -0,0 +1,27 @@
+package exercicios;
+
+import java.awt.event.KeyEvent;
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Ex_04_Soma {
+	public static void main(String[] args) {
+		
+	 Scanner sc = new Scanner(System.in);
+	 
+	 int numeroX;
+	 int numeroY;
+	 
+	 int soma;
+	 
+	 System.out.print("Digite o valor x: ");
+	 numeroX = sc.nextInt();
+	 
+	 System.out.print("Digite o valor y: ");
+	 numeroY = sc.nextInt();
+	 
+	 soma = numeroX +  numeroY;
+	 
+	System.out.println("SOMA: " + soma);
+	}	
+}
