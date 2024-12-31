ae6583ebf6bec00fa5847db66bae1b7decccbf17
Igor Alex de Castro <igoralex8701@gmail.com>
2024-12-31 09:05:20 -0300

    problema nota

--- src/exercicios/estrutura/condicional/Notas.java
+++ src/exercicios/estrutura/condicional/Notas.java
@@ -1,5 +1,31 @@
 package exercicios.estrutura.condicional;
 
-public class Notas {
+import java.util.Locale;
+import java.util.Scanner;
 
+public class Notas {
+	public static void main(String[] args) {
+	   Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+	   double nota1;
+	   double nota2;
+	   double notaFinal;
+	   final double MEDIA = 60.00; 
+	   
+	    System.out.print("Digite a primeira nota:");
+	    nota1 = sc.nextDouble();
+	    
+	    System.out.print("Digite a segunda nota:");
+	    nota2 = sc.nextDouble();
+	    
+	    notaFinal = nota1 + nota2;
+	    
+	    System.out.printf("NOTA FINAL = %.1f \n", notaFinal);
+	    
+	    if(notaFinal < MEDIA) {
+	    	System.out.println("REPROVADO");
+	    }
+	   
+	}
 }
