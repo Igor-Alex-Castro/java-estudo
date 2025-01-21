fea0116427568a1c23ce1f600344addeffde6e62
Igor Alex de Castro <igoralex8701@gmail.com>
2025-01-21 18:57:46 -0300

    ex acima diagonal

--- src/exericios_matrizes/AcimaDiagonal.java
+++ src/exericios_matrizes/AcimaDiagonal.java
@@ -1,5 +1,35 @@
 package exericios_matrizes;
 
-public class AcimaDiagonal {
+import java.util.Scanner;
 
+public class AcimaDiagonal {
+	public static void main(String[] args) {
+		
+		 Scanner scanner = new Scanner(System.in);
+		 
+		 System.out.print("Qual a ordem da matriz? ");
+		 int ordem = scanner.nextInt();
+		 
+		 int[][] matriz = new int[ordem][ordem];
+		 int soma = 0;
+		 for (int i = 0; i < matriz.length; i++) {
+			for (int j = 0; j < matriz[i].length; j++) {
+				System.out.printf("Elemento [%d,%d]:", i, j);
+				matriz[i][j] = scanner.nextInt();
+			}
+		}
+		 
+		 for (int i = 0; i < matriz.length; i++) {
+				for (int j = 0; j < matriz[i].length; j++) {
+					
+					if(matriz[i][j] != 0){
+						if(j > i) {
+							soma += matriz[i][j];
+						}
+					}
+				}
+			}
+		 
+		 System.out.println("SOMA DOS ELEMENTOS ACIMA DA DIAGONAL PRINCIPAL = " + 18) ;
+	}
 }
